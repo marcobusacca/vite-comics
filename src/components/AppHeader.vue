215766b34f8ca884d661871eeb556e146257dd1a
marcobusacca <marcobusacca01@gmail.com>
2023-06-20 15:19:46 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<!-- SCRIPT VUE.JS -->
+<script>
+export default {
+
+}
+</script>
+
+<!-- TEMPLATE HTML -->
+<template lang="">
+  <div>
+
+  </div>
+</template>
+
+<!-- STYLE SCSS -->
+<style lang="scss" scoped></style>

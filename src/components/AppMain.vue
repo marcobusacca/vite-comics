23a2f68cf5b364800a5f6515b86e58424a472d20
marcobusacca <marcobusacca01@gmail.com>
2023-06-21 15:31:47 +0200

    [FIX]

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -134,7 +134,6 @@ main {
                 .card-image {
                     width: 235px;
                     height: 235px;
-                    background-image: url('https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg');
                     background-size: cover;
                     cursor: pointer;
 
